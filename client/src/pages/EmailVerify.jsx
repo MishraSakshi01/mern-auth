7318094eeeaba8fd01b5e2d27421aed224ee2be0
MishraSakshi01 <22amtics112@gmail.com>
2025-03-10 18:13:16 +0530

    user successfully logged in through web and redirect to home page

---
+++ client/src/pages/EmailVerify.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EmailVerify = () => {
+  return (
+    <div><h1>email verify page</h1></div>
+  )
+}
+
+export default EmailVerify
---
+++ client/src/pages/ResetPassword.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ResetPassword = () => {
+  return (
+    <div><h1>Reset password page</h1></div>
+  )
+}
+
+export default ResetPassword
